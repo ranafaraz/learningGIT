46894ca81fa19fa18b7c905e67ceaca85573d043
unknown <ron.hillel.business@gmail.com>
2024-07-07 16:07:43 +0300

    added output for nonce checking at login

--- themes/realhomes/framework/functions/member.php
+++ themes/realhomes/framework/functions/member.php
@@ -60,9 +60,18 @@ if (!function_exists('inspiry_ajax_login')):
 	 */
 	function inspiry_ajax_login()
 	{
-
-		// First check the nonce, if it fails the function will break.
-		check_ajax_referer('inspiry-ajax-login-nonce', 'inspiry-secure-login');
+		try {
+			// First check the nonce, if it fails the function will break.
+			check_ajax_referer('inspiry-ajax-login-nonce', 'inspiry-secure-login');
+		} catch (Exception $e) {
+			echo json_encode(
+				array(
+					'success' => false,
+					'message' => esc_html__('Security check failed!', 'framework'),
+				)
+			);
+			die();
+		}
 
 		if (class_exists('Easy_Real_Estate')) {
 			/* Verify Google reCAPTCHA */
