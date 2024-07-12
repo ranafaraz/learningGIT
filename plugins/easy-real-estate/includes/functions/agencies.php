faf2f091cd9368f7e9f498bc6b5da2cec8a526df
unknown <ron.hillel.business@gmail.com>
2024-07-12 15:42:18 +0300

    fixed agency's properties count function

--- plugins/easy-real-estate/includes/functions/agencies.php
+++ plugins/easy-real-estate/includes/functions/agencies.php
@@ -4,7 +4,7 @@
  */
 
 
-if ( ! function_exists( 'ere_get_agency_properties_count' ) ) {
+if (!function_exists('ere_get_agency_properties_count')) {
 	/**
 	 * Function: Returns the number of listed properties by an agency.
 	 *
@@ -14,61 +14,64 @@ if ( ! function_exists( 'ere_get_agency_properties_count' ) ) {
 	 * @return integer|boolean
 	 *
 	 */
-	function ere_get_agency_properties_count( $agency_id, $post_status = '' ) {
-
+	function ere_get_agency_properties_count($agency_id, $post_status = '')
+	{
 		// Return if agency id is empty.
-		if ( empty( $agency_id ) ) {
+		if (empty($agency_id)) {
 			return false;
 		}
 
-		$agents_query = array(
-			'post_type'      => 'agent',
-			'posts_per_page' => -1,
-			'meta_query'     => array(
-				array(
-					'key'     => 'REAL_HOMES_agency',
-					'value'   => $agency_id,
-					'compare' => '=',
-				),
-			),
+		$agency_post = get_post($agency_id);
+		if (empty($agency_post)) {
+			return false;
+		}
+
+		$user_role = get_user_meta($agency_post->post_author, 'inspiry_user_role', true);
+		if (empty($user_role) || $user_role !== 'agency') {
+			return false;
+		}
+
+		$authors = [$agency_post->post_author];
+
+		// Get all users when inspiry_user_role is agent, and inspiry_user_agency === agency_id
+		$agent_user_ids = get_users(
+			array(
+				'meta_key' => 'inspiry_user_agency',
+				'meta_value' => $agency_id,
+				'fields' => 'ID'
+			)
 		);
 
-		$agent_listing_query = new WP_Query( $agents_query );
-
-		$agent_ids = array();
-
-		if ( $agent_listing_query->have_posts() ) {
-			$agent_ids = wp_list_pluck( $agent_listing_query->posts, 'ID' );
+		// Remove duplicates
+		$agent_user_ids = array_unique($agent_user_ids);
+		if (!empty($agent_user_ids)) {
+			$authors = array_merge($authors, $agent_user_ids);
 		}
 
-		if ( ! empty( $agent_ids ) ) {
+		global $wpdb;
+		$authors_placeholder = implode(',', array_fill(0, count($authors), '%d'));
+		$query = "SELECT COUNT( * ) AS num_posts FROM {$wpdb->posts} WHERE post_type = %s";
+		if (!empty($post_status)) {
+			$query .= " AND post_status = %s";
+		}
+		$query .= " AND post_author IN ($authors_placeholder)";
 
-			// Prepare query arguments.
-			$properties_args = array(
-				'post_type'      => 'property',
-				'posts_per_page' => -1,
-				'meta_query'     => array(
-					array(
-						'key'     => 'REAL_HOMES_agents',
-						'value'   => $agent_ids,
-						'type'    => 'NUMERIC',
-						'compare' => 'IN',
-					),
-				),
-			);
+		// Prepare the query with the appropriate values
+		if (!empty($post_status))
+			$query = $wpdb->prepare($query, array_merge(['property', $post_status], $authors));
+		else
+			$query = $wpdb->prepare($query, array_merge(['property'], $authors));
 
-			// If post status is not empty then add it to the query args.
-			if ( ! empty( $post_status ) ) {
-				$properties_args['post_status'] = $post_status;
-			}
-
-			$properties = new WP_Query( $properties_args );
-			if ( $properties->have_posts() ) {
-				return $properties->found_posts;
-			}
+		$results = (array) $wpdb->get_results($query, ARRAY_A);
+		if (empty($results) || count($results) === 0 || !isset($results[0]['num_posts'])) {
+			return false;
 		}
 
-		return false;
+		$num_posts = (int) $results[0]['num_posts'];
+		if ($num_posts === 0) {
+			return false;
+		}
 
+		return $num_posts;
 	}
 }
